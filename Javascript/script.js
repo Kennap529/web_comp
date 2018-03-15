9e350fd3a2f7dd0bad7a8aca4944d0c397d93008
Kennap529 <kp_529@hotmail.com>
2018-03-15 11:46:01 +0000

    Navigation
    Completed creating navigation between pages

--- Javascript/script.js
+++ Javascript/script.js
@@ -1,4 +1,19 @@
+function home() {
+  location.href = "index.html";
+};
+
+function about() {
+  location.href = "about.html";
+};
+
+function recipes() {
+  location.href = "recipes.html";
+};
+
+function articles() {
+  location.href = "articles.html";
+};
+
 function contact() {
-  /*document.getElementByclass("contact").onclick = function () {*/
   location.href = "contact.html";
 };
